03cd4870c1110ec330716318b21684fb7e3769d9
Jack Lin <jlin@castlighthealth.com>
2019-04-14 21:29:47 -0700

    now python manage.py makemigrations and python manage.py migrate create the first table

--- mainsite/models.py
+++ mainsite/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Attendee(models.Model):
+    chinese_name = models.CharField(max_length=20, db_index=True)
+    english_name = models.CharField(max_length=40, db_index=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=False)
+    updated_at = models.DateTimeField(auto_now=True, blank=False)
+
+    def __str__(self):
+        return '%s %s' % (self.english_name, self.chinese_name)
