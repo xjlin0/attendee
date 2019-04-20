bdcad8f2347c87fa92bfcc1ede3ad836fa65bc3e
Jack Lin <jlin@castlighthealth.com>
2019-04-20 09:52:05 -0700

    add pricing model

--- mainsite/models/__init__.py
+++ mainsite/models/__init__.py
@@ -2,6 +2,7 @@ from .link_note import LinkNote
 from .attendee import Attendee
 from .address import Address
 from .event import Event
+from .price import Price
 from .registration import Registration
 from .attending import Attending
 
---
+++ mainsite/models/price.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+import datetime
+
+from .event import Event
+
+
+class Price(models.Model):
+    price_label = models.CharField(max_length=50, default=str(datetime.date.today().year) + " summer retreat")
+    event = models.ForeignKey(Event, null=True, on_delete=models.SET_NULL)
+    price_type = models.CharField(max_length=20, db_index=True)
+    start_date = models.DateTimeField(auto_now=True, blank=False, db_index=True)
+    price_value = models.DecimalField(max_digits=8, decimal_places=2, default=999999)
+    created_at = models.DateTimeField(auto_now_add=True, blank=False)
+    updated_at = models.DateTimeField(auto_now=True, blank=False)
+
+    def __str__(self):
+        return '%s %s %s %s %s' % (self.event, self.label, self.price_date, self.price_type, self.price_value)
