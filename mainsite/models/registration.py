802b91a19db79a3c1a4ccba6b70546a019e202fd
Jack Lin <jlin@castlighthealth.com>
2019-05-09 05:52:21 -0700

    add donation field for registration

--- mainsite/models/registration.py
+++ mainsite/models/registration.py
@@ -10,6 +10,7 @@ class Registration(models.Model):
     main_attendee = models.ForeignKey(Attendee, null=True, on_delete=models.SET_NULL)
     apply_type = models.CharField(max_length=20, null=True)
     apply_key = models.CharField(max_length=50, null=True)
+    donation = models.DecimalField(max_digits=8, decimal_places=2, default=999999)
     created_at = models.DateTimeField(auto_now_add=True, blank=False)
     updated_at = models.DateTimeField(auto_now=True, blank=False)
 
@@ -22,7 +23,7 @@ class Registration(models.Model):
 
     @property
     def total(self):
-        return sum([attending.price for attending in self.attending_set.all()])
+        return sum([attending.price for attending in self.attending_set.all()]) + self.donation
 
     def __str__(self):
         return '%s %s %s' % (self.apply_type, self.main_attendee, self.total)
--- mainsite/views/attendings.py
+++ mainsite/views/attendings.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.template import loader
+#from django.views.generic import ListView
+
 
 def index(request):
     template = loader.get_template('attendings/index.html')
