9ba7613dc297ce761786365f54d5440c6baade89
Jack Lin <jlin@castlighthealth.com>
2019-05-19 07:07:50 -0700

    switch SECRET_KEY

--- attendee/settings.py
+++ attendee/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'z@%$9!09k#zr%l=%on5@%p^be98pi730tz8^as9i5508)q#cv0'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'z@%$9!09k#zr%l=%on5@%p^be98pi730tz8^as9i5508)q#cv0')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
