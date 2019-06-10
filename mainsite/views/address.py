55f9330dd12f77830f530df3dca0313478db014f
Jack Lin <jlin@castlighthealth.com>
2019-06-10 09:06:31 -0700

    first showed create view form

--- mainsite/views/address.py
+++ mainsite/views/address.py
@@ -1,5 +1,6 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from mainsite.models import Address
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
@@ -16,3 +17,10 @@ class AddressDetailView(DetailView):
     model = Address
     template_name = 'addresses/detail.html'
 
+
+@method_decorator([login_required], name='dispatch')
+class AddressCreateView(CreateView):
+    model = Address
+    fields = ['email1', 'email2', 'phone1', 'phone2', 'address_type', 'street1', 'street2', 'city', 'state', 'zip_code']
+    template_name = 'addresses/create.html'
+    success_url = '/addresses/'
